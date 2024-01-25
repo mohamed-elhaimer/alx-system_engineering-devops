7ca44748109c3222d83db2154776c3ff145ce98e
SimoLogic <mohamedelheimer0@gmail.com>
2024-01-25 18:20:17 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
 # install flask from pip3 within requirement
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
