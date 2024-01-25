08f287fb5b983fcca3947c802b813e5f931789e2
SimoLogic <mohamedelheimer0@gmail.com>
2024-01-25 17:37:09 +0100

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create file in temp with given requirement
+file { '/tmp/school':
+    ensure => 'file',
+    mode => '0744',
+    content => 'I love Puppet',
+    owner => 'www-data',
+    group => 'www-data',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+tasks about configuration managment tools
