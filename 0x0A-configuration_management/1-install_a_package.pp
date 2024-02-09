e10d01d2cec28cb3ea5883b6412488eab0c8de4a
SimoLogic <mohamedelheimer0@gmail.com>
2024-02-09 14:14:48 +0100

    task2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# install flask from pip3
-package { 'flask' :
+# Install flask from pip3
+package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3'
-    }
+}
+
+# Install or upgrade Werkzeug to meet Flask 2.1.0 requirements
+package { 'werkzeug':
+    ensure   => '>=2.1.1',
+    provider => 'pip3'
+}
