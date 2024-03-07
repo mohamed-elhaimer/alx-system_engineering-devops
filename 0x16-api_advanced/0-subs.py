68b854156f7ab2092b249b025111d7e12aa2c1b8
Mohamed EL haimer <mohamedelheimer0@gmail.com>
2024-03-07 17:36:54 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 400:
+    if response.status_code == 404:
         return 0
     result = response.json().get("data")
     return result.get("subscribers")
